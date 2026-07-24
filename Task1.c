fcaf325ef157810c6bbcf9a632df4c51d086309d
Shormishtha <shormishtharaniroy42@gmail.com>
2026-07-24 21:48:18 +0600

    Task 1

---
+++ Task1.c
@@ -0,0 +1,13 @@
+// Task 1: Even or Odd Number
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number:",a);
+    scanf("%d",&a);
+    if(a%2 == 0 )
+    printf("%d is an even number.",a);
+    else
+        printf("%d is an odd number.",a);
+    return 0 ;
+}

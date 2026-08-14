50593c703628f3a8d256d36a2fdf876fc7d4aded
Shormishtha <shormishtharaniroy42@gmail.com>
2026-08-14 06:20:21 +0600

    Task 21

---
+++ Task21.c
@@ -0,0 +1,17 @@
+//  Task 21: Generate Truth Table for XOR Operation
+
+#include<stdio.h> 
+ 
+int main() 
+{ 
+    int A,B; 
+    printf("A B | A XOR B\n"); 
+    for(A=0;A<=1;A++) 
+    { 
+        for(B=0;B<=1;B++) 
+        { 
+            printf("%d %d | %d\n",A,B,A^B); 
+        } 
+    } 
+    return 0; 
+} 
